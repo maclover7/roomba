d27301ea7e1268023e1dba38d9db667091d7a007
maclover7 <me@jonathanmoss.me>
2015-10-03 20:29:16 -0400

    define client constant

--- roomba.rb
+++ roomba.rb
@@ -2,9 +2,17 @@ require 'rubygems'
 require 'octokit'
 
 # Check to make sure ENV variables are setup before running
-REQURED_VARIABLES = ["ROOMBA_GITHUB_USERNAME", "ROOMBA_GITHUB_PASSWORD"]
+REQURED_VARIABLES = ['ROOMBA_GITHUB_USERNAME', 'ROOMBA_GITHUB_PASSWORD']
 REQURED_VARIABLES.each do |var|
   if ENV[var].nil?
     raise ArgumentError, "The #{var} environment variable is not set."
   end
 end
+
+# Get all of the repo's currently open issues
+OCTOKIT_CLIENT = Octokit::Client.new(
+  login: ENV['ROOMBA_GITHUB_USERNAME'],
+  password: ENV['ROOMBA_GITHUB_PASSWORD']
+)
+
+# require 'pry'; binding.pry
