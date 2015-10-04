a5f741d8ce4f0c49841780e7fc94b385d0ee93f0
maclover7 <me@jonathanmoss.me>
2015-10-04 11:10:59 -0400

    Get all open issues for repo, and get last comment on each issue

--- roomba.rb
+++ roomba.rb
@@ -1,8 +1,8 @@
-require 'rubygems'
 require 'octokit'
+require 'active_support/time'
 
 # Check to make sure ENV variables are setup before running
-REQURED_VARIABLES = ['ROOMBA_GITHUB_USERNAME', 'ROOMBA_GITHUB_PASSWORD']
+REQURED_VARIABLES = ['ROOMBA_GITHUB_REPO', 'ROOMBA_GITHUB_USERNAME', 'ROOMBA_GITHUB_PASSWORD']
 REQURED_VARIABLES.each do |var|
   if ENV[var].nil?
     raise ArgumentError, "The #{var} environment variable is not set."
@@ -15,4 +15,21 @@ OCTOKIT_CLIENT = Octokit::Client.new(
   password: ENV['ROOMBA_GITHUB_PASSWORD']
 )
 
-# require 'pry'; binding.pry
+# Retreive all open issues for the given repo.
+issues = OCTOKIT_CLIENT.issues(
+  ENV['ROOMBA_GITHUB_REPO'],
+  state: "open"
+)
+
+# Get a list of all of the numbers of open issues for the given repo.
+issue_numbers = issues.each.map(&:number)
+
+# Get a list of all comments for each open issue, then check if the most recent (last) comment
+# was made before a certain time. If so, comment and close the issue.
+issue_numbers.each do |issue_number|
+  comments = OCTOKIT_CLIENT.issue_comments(ENV['ROOMBA_GITHUB_REPO'], issue_number)
+  last_comment_updated_at_string = comments.last[:updated_at].to_s
+  last_comment_updated_at_time = Time.parse(last_comment_updated_at_string)
+  if last_comment_updated_at_time < 14.days.ago
+  end
+end
