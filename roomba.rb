412f612d382d0f9f5c85094dd44a32f00618e1d8
maclover7 <me@jonathanmoss.me>
2015-10-03 20:13:22 -0400

    Check for required variables

--- roomba.rb
+++ roomba.rb
@@ -1,2 +1,10 @@
 require 'rubygems'
 require 'octokit'
+
+# Check to make sure ENV variables are setup before running
+REQURED_VARIABLES = ["ROOMBA_GITHUB_USERNAME", "ROOMBA_GITHUB_PASSWORD"]
+REQURED_VARIABLES.each do |var|
+  if ENV[var].nil?
+    raise ArgumentError, "The #{var} environment variable is not set."
+  end
+end
